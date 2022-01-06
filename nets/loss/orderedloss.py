d24364af56d16592308b167a7de339e4a8a09bc8
PYsaber <840467890@qq.com>
2022-01-06 16:47:25 +0800

    add

--- nets/loss/orderedloss.py
+++ nets/loss/orderedloss.py
@@ -9,10 +9,11 @@ class OrderedLoss(_Loss):
         self.criterion = nn.CrossEntropyLoss()
         self.alpha = alpha
         self.beta = beta
+        self.epsilon = 0.01 # avoid log(0)
 
     def forward(self, x, target):
         loss_1 = self.criterion(x, target)
         x = F.softmax(x, dim=1)
-        loss_2 = -(x.argmax(1).float()-target.float())**2*(torch.log(1-x.max(1)[0]))
+        loss_2 = -(x.argmax(1).float()-target.float())**2*(torch.log(1-x.max(1)[0]+self.epsilon))
         loss = self.alpha*loss_1+self.beta*loss_2.mean()
         return loss
